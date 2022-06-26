fd114259d8038bbde177b49bfca838255387393b
Golap-mostofa <mostofa6410@gmail.com>
2022-06-26 12:10:06 +0600

    add services

--- index.js
+++ index.js
@@ -12,7 +12,7 @@
 
 
 
-    const uri = "mongodb+srv://dbuser1:4Q4htkkRQnzDCGnB@cluster0.e7veb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+    const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.e7veb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
     const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
     async function run (){
@@ -26,13 +26,20 @@
                 res.send(products)
              })
 
-             app.get('/product:id', async(req, res) =>{
-                 const id = params.id
+             app.get('/product/:id', async(req, res) =>{
+                 const id = req.params.id
                  const query = {_id: ObjectId(id)}
                  console.log(query);
                  const product = await productCollection.findOne(query)
                 
                  res.send(product)
+             })
+
+             //post
+             app.post('/product',async(req,res)=>{
+                const newService = req.body;
+                const result = await productCollection.insertOne(newService)
+                res.send(result)
              })
 
         }
